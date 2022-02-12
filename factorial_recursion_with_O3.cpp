fbd533be2c34ae0b6a3b54bf97a812f861900180
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-02-12 19:19:17 +0530

    factorial program with tail recursion

---
+++ factorial_recursion_with_O3.cpp
@@ -0,0 +1,64 @@
+
+/**
+ * @file factorial_recursion_with_O3.cpp
+ * @author Haramohan Sahu (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+ /*
+
+Types of Recursion:
+There are five types of recursions. They are as follows:
+
+1) Tail Recursion
+2) Head Recursion
+3) Tree Recursion
+4) Indirect Recursion
+5) Nested Recursion
+-----------------------
+
+What does it mean by Tail Recursion?
+If a function is calling itself and that recursive call is the last statement in a function then
+it is called tail recursion. After that call there is nothing, it is not performing anything,
+so, it is called tail recursion. For better understanding
+#include <stdio.h>
+void fun(int n)
+{
+    if (n > 0)
+    {
+        printf("%d", n);
+        fun(n-1);
+    }
+}
+int main ()
+{
+    fun(3);
+    return 0;
+}
+
+
+ */
+#include<iostream>
+using namespace std;
+
+// A tail recursive function to calculate factorial
+unsigned factTR(unsigned int n, unsigned int a)
+{
+    if (n == 1)  return a;
+
+    return factTR(n - 1, n * a);
+}
+
+
+
+// Driver program to test above function
+int main()
+{
+    cout << factTR(3, 1);
+    return 0;
+}
